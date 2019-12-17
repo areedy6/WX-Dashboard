8ed7537f3b26ea9c44a40d00dab002f142b9ecf2
andrewreedy <reedyandrew11@gmail.com>
2019-12-16 21:11:36 -0500

    working button

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
+var APIKey = "166a433c57516f51dfab1f7edaed8413";
+   
+$("#button").on("click", function() {
 
-// function displayWXInfo() {
-var APIKey = "e9f0680aefb142c325206fad739a2385";
-// var inputCity = "(#button)"
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=cleveland&appid=" + APIKey;
+var cityName = $("input:text").val();
+document.getElementById("weather-input").innerHTML = cityName; 
+
+
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=cleveland&appid=" + APIKey;
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
 
 $.ajax({
     url: queryURL,
@@ -12,30 +18,102 @@ $.ajax({
     console.log(queryURL);
     console.log(response);
 
-    var wxDiv = $("<div class='weather'>");
-    
+
+    var wxDiv = $("<div>");
     var city = response.name;
-
     var pOne = $("<p>").text("City: " + city);
-
-    wxDiv.append(pOne);
-
-    console.log(city)
-
     var wind = response.wind.speed;
-
     var pTwo = $("<p>").text("Wind Speed: " + wind);
+    var humid = response.main.humidity;
+    var pThree = $("<p>").text("Humidity: " + humid);
+    var temp = response.main.temp;
+    var pFour = $("<p>").text("Temperature: " + temp);
 
-    wxDiv.append(pTwo);
-
-    console.log(wind)
-
-
-
-    // var hummidDiv = $("<div class='weather'>");
-
-    // var tempDiv = $("<div class='weather'>");
     
-    // var uvDiv = $("<div class='weather'>");
+    
+    wxDiv.append(pOne);
+    console.log(city)
+    wxDiv.append(pTwo);
+    console.log(wind)
+    wxDiv.append(pThree);
+    console.log(humid)
+    wxDiv.append(pFour);
+    console.log(temp)
+
+
+  $("#weather").prepend(wxDiv)
+
+  })
 })
+
+
+
+
+    // var wxDiv = $("<div class='weather'>");
+    
+    // var city = response.name;
+
+    // var pOne = $("<p>").text("City: " + city);
+
+    // wxDiv.append(pOne);
+
+    // console.log(city)
+
+    // var wind = response.wind.speed;
+
+    // var pTwo = $("<p>").text("Wind Speed: " + wind);
+
+    // wxDiv.append(pTwo);
+
+    // console.log(wind)
+
+    // var humid = response.main.humidity;
+
+    // var pThree = $("<p>").text("Humidity: " + humid);
+
+    // wxDiv.append(pThree);
+
+    // console.log(humid);
+
+    // var temp = response.main.temp;
+
+    // var pFour = $("<p>").text("Temperature: " + temp);
+
+    // wxDiv.append(pFour);
+
+    //     // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    //     //  $(".tempF").text("Temperature (Kelvin) " + tempF);
+
+    // console.log(temp);
+
+    // $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+    // $(".wind").text("Wind Speed: " + response.wind.speed);
+    // $(".humidity").text("Humidity: " + response.main.humidity);
+    // $(".temp").text("Temperature (F) " + response.main.temp);
+    
+// })
 // }
+
+
+    $("#button").on("click", function() {
+
+    var cityName = $("input:text").val();
+    document.getElementById("weather-input").innerHTML = cityName; 
+    // Here we are building the URL we need to query the database
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" + cityName + "&appid=" + APIKey;
+    //   "q=Bujumbura,Burundi&units=imperial&appid=" + APIKey;
+    
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        // We store all of the retrieved data inside of an object called "response"
+        .then(function(response) {
+  
+          // Log the queryURL
+          console.log(queryURL);
+  
+          // Log the resulting object
+          console.log(response);
+        })})

0444b8544d2ee52b10a8daf99050516fbc9ecd5a
andrewreedy <reedyandrew11@gmail.com>
2019-12-19 12:43:17 -0500

    search button

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $("#search-button").on("click", function() {
     var searchValue = $("#search-value").val();
 
-    // clear input box
     $("#search-value").val("");
 
     searchWeather(searchValue);
@@ -23,7 +22,7 @@ $(document).ready(function() {
       url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=166a433c57516f51dfab1f7edaed8413",
       dataType: "json",
       success: function(data) {
-        // create history link for this search
+  
         if (history.indexOf(searchValue) === -1) {
           history.push(searchValue);
           window.localStorage.setItem("history", JSON.stringify(history));
@@ -31,10 +30,8 @@ $(document).ready(function() {
           makeRow(searchValue);
         }
         
-        // clear any old content
         $("#today").empty();
 
-        // create html content for current weather
         var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
         var card = $("<div>").addClass("card");
         var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
@@ -43,13 +40,11 @@ $(document).ready(function() {
         var cardBody = $("<div>").addClass("card-body");
         var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
 
-        // merge and add to page
         title.append(img);
         cardBody.append(title, temp, humid, wind);
         card.append(cardBody);
         $("#today").append(card);
 
-        // call follow-up api endpoints
         getForecast(searchValue);
         getUVIndex(data.coord.lat, data.coord.lon);
       }
@@ -115,20 +110,20 @@ $(document).ready(function() {
 // })
 
 // FIVE DAY FORECAST
+
   function getForecast(searchValue) {
     $.ajax({
       type: "GET",
       url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=166a433c57516f51dfab1f7edaed8413",
       dataType: "json",
       success: function(data) {
-        // overwrite any existing content with title and empty row
+        
         $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
 
-        // loop over all forecasts (by 3-hour increments)
         for (var i = 0; i < data.list.length; i++) {
-          // only look at forecasts around 3:00pm
+          
           if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
-            // create html elements for a bootstrap card
+            
             var col = $("<div>").addClass("col-md-2");
             var card = $("<div>").addClass("card bg-primary text-white");
             var body = $("<div>").addClass("card-body p-2");
@@ -140,7 +135,6 @@ $(document).ready(function() {
             var p1 = $("<p>").addClass("card-text").text("Temp: " + data.list[i].main.temp_max + " °F");
             var p2 = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
 
-            // merge together and put on page
             col.append(card.append(body.append(title, img, p1, p2)));
             $("#forecast .row").append(col);
           }
@@ -159,8 +153,7 @@ $(document).ready(function() {
       success: function(data) {
         var uv = $("<p>").text("UV Index: ");
         var btn = $("<span>").addClass("btn btn-sm").text(data.value);
-        
-        // change color depending on uv value
+      
         if (data.value < 3) {
           btn.addClass("btn-success");
         }
